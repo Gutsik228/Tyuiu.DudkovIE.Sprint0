dcce8ce7793f5ab79283d764f44c0ad63dc63419
Gutsik228 <igordudkov22@gmail.com>
2023-09-26 17:13:02 +0500

    Task1

---
+++ Tyuiu.DudkovIE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.DudkovIE.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            
+            Console.WriteLine("Введите свое имя");
+            string Name;
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Введите свой возраст");
+            int age;
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Здравствуйте, меня зовут: " + Name +  " " + "Мой возраст: " + age);
+
+        }
+    }
+}
